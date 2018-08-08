74bc667f3d21703e21e2e05964ddc668c0ccd87b
Rafał Lipieta <cwgflojdid@wp.pl>
2018-08-08 14:32:44 +0200

    Add comments adn reducer.js

--- actions.js
+++ actions.js
@@ -1,10 +1,10 @@
 import uuid from uuid;
 
-const ADD_COMMENT = 'ADD_COMMENT';
-const REMOVE_COMMENT = 'REMOVE_COMMENT';
-const EDIT_COMMENT = 'EDIT_COMMENT';
-const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
-const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+export const ADD_COMMENT = 'ADD_COMMENT';
+export const REMOVE_COMMENT = 'REMOVE_COMMENT';
+export const EDIT_COMMENT = 'EDIT_COMMENT';
+export const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
+export const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
 
 function addComment(text) {
     return {
@@ -41,24 +41,24 @@ const boundRemoveComment = id => dispatch(removeComment(id));
 
 ////////////////////////////////////////////////////////////////////////
 
-function thumbUpComment(id, value) {
+function thumbUpComment(id, votes) {
     return {
         type: THUMB_UP_COMMENT,
         id,
-        value: +1
+        votes: +1
     }
 }
 
-const boundThumbUpComment = (id, value) => dispatch(thumbUpComment(id, value));
+const boundThumbUpComment = (id, votes) => dispatch(thumbUpComment(id, votes));
 
 ////////////////////////////////////////////////////////////////////////
 
-function thumbUpComment(id, value) {
+function thumbUpComment(id, votes) {
     return {
         type: THUMB_DOWN_COMMENT,
         id,
-        value: -1
+        votes: -1
     }
 }
 
-const boundThumbDownComment = (id, value) => dispatch(thumbDownComment(id, value));
+const boundThumbDownComment = (id, votes) => dispatch(thumbDownComment(id, votes));
